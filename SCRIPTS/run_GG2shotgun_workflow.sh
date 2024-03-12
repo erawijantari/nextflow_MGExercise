ccb14e763f0e493a5ddea3026f6438788d7c340d
Pande Erawijantari <erawijan@puhti-login15.bullx>
2024-03-12 23:30:21 +0200

    edit path

--- SCRIPTS/run_GG2shotgun_workflow.sh
+++ SCRIPTS/run_GG2shotgun_workflow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load snakemake/7.15.2
+export PATH="/projappl/project_2005073/erawijan/project/qiime2-shotgun-2023.09/bin:$PATH"
 
 snakemake  -s ./workflow/Snakefile_gg2_shotgun  --cluster \
     " sbatch --account=project_2005073 \
