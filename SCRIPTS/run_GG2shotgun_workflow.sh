36eae3102bdb4b4ca0642f113a38f845cbb50176
Pande Erawijantari <erawijan@puhti-login14.bullx>
2024-03-14 00:28:53 +0200

    load the correct snakemake version

--- SCRIPTS/run_GG2shotgun_workflow.sh
+++ SCRIPTS/run_GG2shotgun_workflow.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-module load snakemake/7.15.2
-export PATH="/projappl/project_2005073/erawijan/project/qiime2-shotgun-2023.09/bin:$PATH"
+module load snakemake/7.17.1
+#module load snakemake
+source ~/.bashrc
+source ~/.bash_profile
+
+
 
 snakemake  -s ./workflow/Snakefile_gg2_shotgun  --cluster \
     " sbatch --account=project_2005073 \
     --partition small --mem 150G --time=2-00:00:00 \
     --ntasks=1 --cpus-per-task=32 \
      -e LOGS/shotgun_err_%A_%a.txt" -j 400 \
-    --stats ./comp_jobs_stats.json --keep-going --rerun-incomplete
+     --stats ./comp_jobs_stats.json --keep-going --rerun-incomplete
+
+
+#snakemake  -s ./workflow/Snakefile_gg2_shotgun  --cluster-generic-submit-cmd \
+#    " sbatch --account=project_2005073 \
+#    --partition small --mem 150G --time=2-00:00:00 \
+#    --ntasks=1 --cpus-per-task=32 \
+#     -e LOGS/shotgun_err_%A_%a.txt" -j 400 \
+#      --keep-going --rerun-incomplete     
